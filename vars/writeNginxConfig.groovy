22d02a57001a33efbfb859cc35a525afde5fb505
ruos-sovanra <vannraruos@gmail.com>
2024-10-02 21:17:23 +0700

    update code config link folder

--- vars/writeNginxConfig.groovy
+++ vars/writeNginxConfig.groovy
@@ -7,6 +7,20 @@ def call(String subdomain, String domain, String deployPort) {
     def filePath = "/etc/nginx/sites-available/${folderName}"
     def symlinkPath = "/etc/nginx/sites-enabled/"
 
+
+    echo >> "server {
+                     listen 80;
+                     server_name ${subdomain}.${domain};
+
+                     location / {
+                         proxy_pass http://localhost:${deployPort};
+                         proxy_set_header Host \$host;
+                         proxy_set_header X-Real-IP \$remote_addr;
+                         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                         proxy_set_header X-Forwarded-Proto \$scheme;
+                     }
+                 } " > ${filePath}
+
     def nginxConfig = """
     server {
         listen 80;
