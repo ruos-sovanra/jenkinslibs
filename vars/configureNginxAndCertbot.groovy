e577f8264ff3011ca1ac9b94993465208f2a24ce
ruos-sovanra <vannraruos@gmail.com>
2024-10-02 21:52:02 +0700

    update code config link folder

--- vars/configureNginxAndCertbot.groovy
+++ vars/configureNginxAndCertbot.groovy
@@ -1,8 +1,10 @@
 def call(String subdomain, String domain, String deployPort) {
+    // Check if the variables are passed
     if (!subdomain || !domain || !deployPort) {
         error "subdomain, domain, and deployPort must be provided"
     }
 
+    // Echo the variables to check if they are passed down correctly
     echo "Subdomain: ${subdomain}"
     echo "Domain: ${domain}"
     echo "Deploy Port: ${deployPort}"
@@ -11,5 +13,27 @@ def call(String subdomain, String domain, String deployPort) {
         error "subdomain, domain, and deployPort cannot be empty"
     }
 
-    writeNginxConfig(subdomain, domain, deployPort)
+    // Use double quotes for the sh block to allow Groovy variable interpolation
+    sh """
+    #!/bin/bash
+
+    folder_name="${subdomain}.${domain}"
+    file_path="/etc/nginx/sites-available/\${folder_name}"
+
+    # Write the specific Nginx configuration to the file
+    cat  << EOF > \${file_path}
+    server {
+        listen 80;
+        server_name ${subdomain}.${domain};
+
+        location / {
+            proxy_pass http://localhost:${deployPort};
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+    }
+    EOF
+    """
 }
